a5f4db17cd2c92e995f0bd29a0059ba9e8e3fbaa
James <jfnelson2000@gmail.com>
2023-04-28 00:00:50 -0700

    Done

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Counter from './Components/Counter';
+import Todo from './Components/Todo';
+
+function App() {
+  return (
+    <div className="App">
+      <Counter />
+      <Todo />
+    </div>
+  );
+}
+
+export default App;
